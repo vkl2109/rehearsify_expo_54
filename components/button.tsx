c4d9ac15248e86137092854f1fc0de3d40f235c4
Vincent Le <vkl2109@columbia.edu>
2025-11-11 10:48:19 -0500

    Created components folder with initial button

---
+++ components/button.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, View } from 'react-native';
+
+
+export default function Button() {
+    return(
+        <View style={{...styles.btnContainer, 
+
+        }}>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  btnContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
